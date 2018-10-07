1f514487aa185b71cb8bfd538bd3801eb007a45a
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-07 19:12:20 +0300

    Create IClassMemberDeclaration interface

---
+++ AssemblyBrowser/IClassMemberDeclaration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser
+{
+    interface IClassMemberDeclaration
+    {
+        List<string> Attributes { get; }
+        List<string> Modifiers { get; }
+        string Type { get; }
+        string Name { get; }
+    }
+}
