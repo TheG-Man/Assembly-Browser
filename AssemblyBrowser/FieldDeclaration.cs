b7fdf9874aee50cd5785f0524832f5740a3dae45
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-07 19:12:49 +0300

    Create FieldDeclaration class

---
+++ AssemblyBrowser/FieldDeclaration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser
+{
+    internal class FieldDeclaration: IClassMemberDeclaration
+    {
+        public List<string> Attributes { get; }
+        public List<string> Modifiers { get; }
+        public string Type { get; }
+        public string Name { get; }
+
+        internal FieldDeclaration(object[] attributes, object[] modifiers, string fieldType, string fieldName)
+        {
+            Attributes = new List<string>();
+            Attributes.AddRange((IEnumerable<string>)attributes);
+
+            Modifiers = new List<string>();
+            Modifiers.AddRange((IEnumerable<string>)modifiers);
+
+            Type = fieldType;
+            Name = fieldName;
+        }
+    }
+}
