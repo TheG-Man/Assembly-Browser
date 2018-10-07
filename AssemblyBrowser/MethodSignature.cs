5dc6a9d17492a53a028e53c4d3b5e37b31d51954
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-07 19:14:04 +0300

    Create MethodSignature class

---
+++ AssemblyBrowser/MethodSignature.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser
+{
+    internal class MethodSignature
+    {
+        internal string AccessLevel { get; }
+        internal List<string> Modifiers { get; }
+        internal string ReturnValue { get; }
+        internal string MethodName { get; }
+        internal List<string> Parameters { get; }
+
+        internal MethodSignature(string accessLevel, object[] modifiers, string returnValue, string methodName, object[] parameters)
+        {
+            AccessLevel = accessLevel;
+
+            Modifiers = new List<string>();
+            Modifiers.AddRange((IEnumerable<string>)modifiers);
+
+            ReturnValue = returnValue;
+            MethodName = methodName;
+
+            Parameters = new List<string>();
+            Parameters.AddRange((IEnumerable<string>)parameters);
+        }
+    }
+}
