ef0422c77bc992fedcc286f5ec1e4da62696a38d
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-19 22:37:47 +0300

    Add AssemblyBrowser implementation

--- AssemblyBrowser/AssemblyBrowser.cs
+++ AssemblyBrowser/AssemblyBrowser.cs
@@ -4,11 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using AssemblyBrowser.Builders;
 
 namespace AssemblyBrowser
 {
-    public class AssemblyBrowser
+    public class AssemblyBrowser : IAssemblyBrowser
     {
-        
+        private static volatile AssemblyBrowser _instance;
+        private static readonly object _syncRoot = new object();
+
+        private readonly AssemblyInfo _assemblyInfo;
+
+        private AssemblyBrowser()
+        {
+            _assemblyInfo = new AssemblyInfo();
+        }
+
+        public static AssemblyBrowser GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_syncRoot)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new AssemblyBrowser();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public AssemblyInfo GetAssemblyInfo(string path)
+        {
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(path);
+
+                foreach (TypeInfo definedType in assembly.DefinedTypes)
+                {
+                    if (!definedType.IsNested)
+                    {
+                        NamespaceDeclaration namespaceDeclaration = new NamespaceDeclaration(definedType.Namespace);
+
+                        var buildDirector = new BuildDirector(new TypeBuilder(definedType));
+                        TypeDeclaration typeDeclaration = (TypeDeclaration)buildDirector.Construct();
+
+                        namespaceDeclaration.AddType(typeDeclaration);
+                        _assemblyInfo.AddNamespace(namespaceDeclaration);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //Assembly load exception
+            }
+
+            return _assemblyInfo;
+            }
     }
 }
