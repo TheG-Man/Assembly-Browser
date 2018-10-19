f310b3b0bb7b3421641ce69ce8914f3dc265b87e
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-19 22:34:46 +0300

    Add AssemblyInfo implementation

--- AssemblyBrowser/AssemblyInfo.cs
+++ AssemblyBrowser/AssemblyInfo.cs
@@ -8,6 +8,30 @@ namespace AssemblyBrowser
 {
     public sealed class AssemblyInfo
     {
+        private readonly List<NamespaceDeclaration> _namespaces;
 
+        public IEnumerable<NamespaceDeclaration> Namespaces => _namespaces;
+
+        internal AssemblyInfo()
+        {
+            _namespaces = new List<NamespaceDeclaration>();
+        }
+
+        internal void AddNamespace(NamespaceDeclaration namespaceDeclaration)
+        {
+            NamespaceDeclaration ns = _namespaces.Find(n => n.Name == namespaceDeclaration.Name);
+
+            if (ns != null)
+            {
+                foreach (TypeDeclaration typeDeclaration in namespaceDeclaration.Types)
+                {
+                    ns.AddType(typeDeclaration);
+                }
+            }
+            else
+            {
+                _namespaces.Add(namespaceDeclaration);
+            }
+        }
     }
 }
