6265e3fc600c8fda6491c19f95cc2e3784060f29
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-19 23:00:52 +0300

    Create PropertyModifiers class

---
+++ AssemblyBrowser/TypeMembers/PropertyModifiers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser.TypeMembers
+{
+    public class PropertyModifiers : Modifiers
+    {
+        private readonly List<string> _getterModifiers;
+        private readonly List<string> _setterModifiers;
+
+        public IEnumerable<string> GetterModifiers => _getterModifiers;
+        public IEnumerable<string> SetterModifiers => _setterModifiers;
+
+        public PropertyModifiers(List<string> dotnetModifiers, List<string> csharpModifiers, List<string> getterModifiers, List<string> setterModifiers)
+            :base(dotnetModifiers, csharpModifiers)
+        {
+            _getterModifiers = getterModifiers;
+            _setterModifiers = setterModifiers;
+        }
+    }
+}
