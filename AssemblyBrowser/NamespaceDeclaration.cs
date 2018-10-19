d44ca81a4778b1779928bb18f138ad57aec3c6ff
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-19 22:56:58 +0300

    Create NamespaceDeclaration class

---
+++ AssemblyBrowser/NamespaceDeclaration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser
+{
+    public class NamespaceDeclaration
+    {
+        private readonly List<TypeDeclaration> _types;
+
+        public string Name { get; }
+        public IEnumerable<TypeDeclaration> Types => _types;
+
+        public NamespaceDeclaration(string name)
+        {
+            Name = name;
+
+            _types = new List<TypeDeclaration>();
+        }
+
+        public void AddType(TypeDeclaration typeDeclaration)
+        {
+            _types.Add(typeDeclaration);
+        }
+    }
+}
