3986f457792eb79d133e0038130106a93fdad0b7
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-19 22:58:19 +0300

    Create EventDeclaration class

---
+++ AssemblyBrowser/TypeMembers/EventDeclaration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser.TypeMembers
+{
+    public class EventDeclaration : ITypeMemberDeclaration
+    {
+        private readonly Modifiers _modifiers;
+
+        public Modifiers Modifiers => _modifiers;
+        public string TypeName { get; }
+        public string Name { get; }
+
+        public EventDeclaration(string name, string typeName, Modifiers modifiers)
+        {
+            Name = name;
+            TypeName = typeName;
+
+            _modifiers = modifiers;
+        }
+    }
+}
