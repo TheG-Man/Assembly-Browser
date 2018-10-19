20036186222d951e98ec7444389a757cce6b642f
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-19 22:40:02 +0300

    Create BuildDirector class

---
+++ AssemblyBrowser/Builders/BuildDirector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser.Builders
+{
+    class BuildDirector
+    {
+        private readonly IBuilder _builder;
+
+        public BuildDirector(IBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public object Construct()
+        {
+            return _builder.Build();
+        }
+    }
+}
