3757e7ee49a88ff9fe102a6a72577ba181b78bf4
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-19 23:00:37 +0300

    Create Modifiers class

---
+++ AssemblyBrowser/TypeMembers/Modifiers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser.TypeMembers
+{
+    public class Modifiers
+    {
+        private readonly List<string> _dotnetModifiers;
+        private readonly List<string> _csharpModifiers;
+
+        public IEnumerable<string> DotnetModifiers => _dotnetModifiers;
+        public IEnumerable<string> CSharpModifiers => _csharpModifiers;
+
+        public Modifiers(List<string> dotnetModifiers, List<string> csharpModifiers)
+        {
+            _dotnetModifiers = dotnetModifiers;
+            _csharpModifiers = csharpModifiers;
+        }
+    }
+}
