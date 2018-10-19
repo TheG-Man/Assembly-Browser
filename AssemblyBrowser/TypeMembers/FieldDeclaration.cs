b3ec2a59ca92f025c218ffedd0d64c51272957e2
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-19 22:58:36 +0300

    Create FieldDeclaration class

---
+++ AssemblyBrowser/TypeMembers/FieldDeclaration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AssemblyBrowser.TypeMembers
+{
+    public class FieldDeclaration: ITypeMemberDeclaration, IGeneric
+    {
+        private readonly Modifiers _modifiers;
+        private readonly List<string> _genericParameters;
+
+        public Modifiers Modifiers => _modifiers;
+        public IEnumerable<string> GenericParameters => _genericParameters;
+        public string TypeName { get; }
+        public string Name { get; }
+        public bool IsGeneric { get; }
+
+        public FieldDeclaration(string name, string typeName, bool isGeneric, Modifiers modifiers, List<string> genericParameters)
+        {
+            Name = name;
+            TypeName = typeName;
+            IsGeneric = isGeneric;
+
+            _modifiers = modifiers;
+            _genericParameters = genericParameters;
+        }
+    }
+}
