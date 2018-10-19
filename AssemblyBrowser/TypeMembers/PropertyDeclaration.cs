4d23c40a75c08853a0999b72de1d66af0e83529b
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-19 22:59:27 +0300

    Create PropertyDeclaration class

---
+++ AssemblyBrowser/TypeMembers/PropertyDeclaration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser.TypeMembers
+{
+    public class PropertyDeclaration: ITypeMemberDeclaration
+    {
+        private readonly Modifiers _modifiers;
+        private readonly List<string> _genericParameters;
+
+        public string TypeName { get; }
+        public string Name { get; }
+        public bool IsGeneric { get; }
+        public bool CanRead { get; }
+        public bool CanWrite { get; }
+        public Modifiers Modifiers => _modifiers;
+        public IEnumerable<string> GenericParameters => _genericParameters;
+
+        public PropertyDeclaration(string name, string typeName, bool isGeneric, bool canRead, bool canWrite, PropertyModifiers modifiers, List<string> genericParameters)
+        {
+            Name = name;
+            TypeName = typeName;
+            IsGeneric = isGeneric;
+            CanRead = canRead;
+            CanWrite = canWrite;
+
+            _modifiers = modifiers;
+            _genericParameters = genericParameters;
+        }
+    }
+}
