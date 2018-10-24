547ec06d33add4f45deae6b1f3deb5ed9d433f77
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-24 12:43:21 +0300

    Add BuilderExtensions class

--- AssemblyBrowser/Builders/IBuilder.cs
+++ AssemblyBrowser/Builders/IBuilder.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AssemblyBrowser.TypeMembers;
 
 namespace AssemblyBrowser.Builders
 {
     interface IBuilder
     {
         object Build();
+    }
+
+    public static class BuilderExtensions
+    {
+        public static object Build(this TypeBuilder typeBuilder, Dictionary<string, MethodDeclaration> extensionMethods)
+        {
+            return typeBuilder.Build(extensionMethods);
+        }
     }
 }
