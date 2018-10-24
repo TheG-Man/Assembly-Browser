aa8182383ddc83b55f6f280f29c1a030ea49df0c
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-24 12:35:19 +0300

    Add extension methods support

--- AssemblyBrowser/AssemblyBrowser.cs
+++ AssemblyBrowser/AssemblyBrowser.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using AssemblyBrowser.TypeMembers;
 using AssemblyBrowser.Builders;
 
 namespace AssemblyBrowser
@@ -13,11 +14,11 @@ namespace AssemblyBrowser
         private static volatile AssemblyBrowser _instance;
         private static readonly object _syncRoot = new object();
 
-        private readonly AssemblyInfo _assemblyInfo;
+        private readonly List<string> _typesWithExtensionMethods = new List<string>();
 
         private AssemblyBrowser()
         {
-            _assemblyInfo = new AssemblyInfo();
+           
         }
 
         public static AssemblyBrowser GetInstance()
@@ -38,30 +39,68 @@ namespace AssemblyBrowser
 
         public AssemblyInfo GetAssemblyInfo(string path)
         {
+            var assemblyInfo = new AssemblyInfo();
+
             try
             {
                 Assembly assembly = Assembly.LoadFrom(path);
+                Dictionary<string, MethodDeclaration> extensionMethods = GetExtensionMethods(assembly);
 
                 foreach (TypeInfo definedType in assembly.DefinedTypes)
                 {
                     if (!definedType.IsNested)
                     {
-                        NamespaceDeclaration namespaceDeclaration = new NamespaceDeclaration(definedType.Namespace);
+                        if (!_typesWithExtensionMethods.Contains(definedType.Name))
+                        {
+                            NamespaceDeclaration namespaceDeclaration = new NamespaceDeclaration(definedType.Namespace);
 
-                        var buildDirector = new BuildDirector(new TypeBuilder(definedType));
-                        TypeDeclaration typeDeclaration = (TypeDeclaration)buildDirector.Construct();
+                            var buildDirector = new BuildDirector(new TypeBuilder(definedType));
+                            TypeDeclaration typeDeclaration = (TypeDeclaration)buildDirector.Construct(extensionMethods);
 
-                        namespaceDeclaration.AddType(typeDeclaration);
-                        _assemblyInfo.AddNamespace(namespaceDeclaration);
+                            namespaceDeclaration.AddType(typeDeclaration);
+                            assemblyInfo.AddNamespace(namespaceDeclaration);
+                        }
                     }
                 }
+
+                _typesWithExtensionMethods.Clear();
             }
             catch (Exception e)
             {
-                //Assembly load exception
+                throw e;
             }
 
-            return _assemblyInfo;
+            return assemblyInfo;
+        }
+
+        private Dictionary<string, MethodDeclaration> GetExtensionMethods(Assembly assembly)
+        {
+            Dictionary<string, MethodDeclaration> extensionMethods = new Dictionary<string, MethodDeclaration>();
+
+            foreach (TypeInfo definedType in assembly.DefinedTypes)
+            {
+                if (!definedType.IsNested)
+                {
+                    foreach (MethodInfo method in definedType.DeclaredMethods)
+                    {
+                        ParameterInfo[] parameters = method.GetParameters();
+                        Type parameterType = parameters.Count() > 0 ? parameters.First().ParameterType : null;
+
+                        if (parameterType != null)
+                        {
+                            if (method.IsStatic && (parameterType.IsClass || parameterType.IsInterface))
+                            {
+                                _typesWithExtensionMethods.Add(definedType.Name);
+
+                                var buildDirector = new BuildDirector(new MethodBuilder(method));
+                                extensionMethods.Add(parameterType.Name, (MethodDeclaration)buildDirector.Construct());
+                            }
+                        }
+                    }
+                }
             }
+
+            return extensionMethods;
+        }
     }
 }
