bcef80252bd533d8333eb5a375daabf22daf1b7e
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-25 15:34:02 +0300

    Create AssemblyBrowserModel class
    This class is a model layer of MVVM pattern. It implements INotifyPropertyChanged interface to notify a ViewModel layer about some changes.

---
+++ AssemblyBrowserWPF/Model/AssemblyBrowserModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using AssemblyBrowserWPF.ViewModel;
+using AssemblyBrowser;
+using AssemblyBrowser.TypeMembers;
+
+namespace AssemblyBrowserWPF.Model
+{
+    class AssemblyBrowserModel : INotifyPropertyChanged
+    {
+        private readonly IAssemblyBrowser _assemblyBrowser = AssemblyBrowser.AssemblyBrowser.GetInstance();
+
+        private AssemblyViewModel _assemblyInfoViewModel;
+        public AssemblyViewModel AssemblyViewModelInfo
+        {
+            get
+            {
+                return _assemblyInfoViewModel;
+            }
+            private set
+            {
+                if (_assemblyInfoViewModel != value)
+                {
+                    _assemblyInfoViewModel = value;
+                    RaisePropertyChanged("AssemblyViewModelInfo");
+                }
+            }
+        }
+
+        public AssemblyBrowserModel()
+        {
+
+        }
+
+        public void OpenAssembly(string path)
+        {
+            try
+            {
+                AssemblyInfo assemblyInfo = _assemblyBrowser.GetAssemblyInfo(path);
+                AssemblyViewModelInfo = new AssemblyViewModel(assemblyInfo);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        void RaisePropertyChanged(string prop)
+        {
+            PropertyChangedEventHandler propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
