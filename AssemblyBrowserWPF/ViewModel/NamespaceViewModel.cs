1c2755b8b1da4369a5ddd47cdb358286706f20b1
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-25 15:37:45 +0300

    Create NamespaceViewModel class

---
+++ AssemblyBrowserWPF/ViewModel/NamespaceViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssemblyBrowser;
+
+namespace AssemblyBrowserWPF.ViewModel
+{
+    class NamespaceViewModel
+    {
+        private readonly NamespaceDeclaration _namespaceDeclaration;
+
+        public string StringRepresentation => _namespaceDeclaration.Name;
+        public IEnumerable<TypeViewModel> Types
+        {
+            get
+            {
+                foreach (TypeDeclaration typeDeclaration in _namespaceDeclaration.Types)
+                {
+                    yield return new TypeViewModel(typeDeclaration);
+                }
+            }
+        }
+
+        public NamespaceViewModel(NamespaceDeclaration namespaceDeclaration)
+        {
+            _namespaceDeclaration = namespaceDeclaration;
+        }
+    }
+}
