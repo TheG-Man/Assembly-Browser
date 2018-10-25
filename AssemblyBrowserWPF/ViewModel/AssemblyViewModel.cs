edf3ceaea3496cf84114939c19591f3f0c8e8c9c
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-25 15:37:28 +0300

    Create AssemblyViewModel class

---
+++ AssemblyBrowserWPF/ViewModel/AssemblyViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssemblyBrowser;
+
+namespace AssemblyBrowserWPF.ViewModel
+{
+    class AssemblyViewModel
+    {
+        private readonly AssemblyInfo _assemblyInfo;
+
+        public string StringRepresentation => GetStringRepresentation();
+        public IEnumerable<NamespaceViewModel> Namespaces
+        {
+            get
+            {
+                foreach (NamespaceDeclaration ns in _assemblyInfo.Namespaces)
+                {
+                    yield return new NamespaceViewModel(ns);
+                }
+            }
+        }
+
+        public AssemblyViewModel(AssemblyInfo assemblyInfo)
+        {
+            _assemblyInfo = assemblyInfo;
+        }
+
+        private string GetStringRepresentation()
+        {
+            return "";
+        }
+    }
+}
