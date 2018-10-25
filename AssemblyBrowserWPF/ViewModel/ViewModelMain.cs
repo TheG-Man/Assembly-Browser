4a12575f96c16948bd355ed00c511e3b46f4d904
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-25 15:36:53 +0300

    Create ViewModelMain class
    This class is a viewmodel layer of MVVM pattern.

---
+++ AssemblyBrowserWPF/ViewModel/ViewModelMain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Microsoft.Win32;
+using AssemblyBrowserWPF.Model;
+using AssemblyBrowser;
+
+namespace AssemblyBrowserWPF.ViewModel
+{
+    class ViewModelMain : INotifyPropertyChanged
+    {
+        private AssemblyBrowserModel _assemblyBrowserModel;
+
+        public AssemblyViewModel AssemblyViewModelInfo => _assemblyBrowserModel.AssemblyViewModelInfo;
+        public RelayCommand OpenAssemblyCommand { get; set; }
+
+        public ViewModelMain()
+        {
+            _assemblyBrowserModel = new AssemblyBrowserModel();
+
+            OpenAssemblyCommand = new RelayCommand(OpenAssembly);
+            _assemblyBrowserModel.PropertyChanged += (s, e) => { RaisePropertyChanged(e.PropertyName); };
+        }
+
+        void OpenAssembly(object parameter)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog().Value)
+            {
+                _assemblyBrowserModel.OpenAssembly(openFileDialog.FileName);
+            }
+        }
+
+        internal void RaisePropertyChanged(string prop)
+        {
+            PropertyChangedEventHandler propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
