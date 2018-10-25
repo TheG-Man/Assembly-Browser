1d9316c09113d93619d86eab4e723a79bd20a3f0
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-10-25 16:16:10 +0300

    Create LoadAssemblyException class

---
+++ AssemblyBrowser/LoadAssemblyException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser
+{
+    public class LoadAssemblyException : Exception
+    {
+        public LoadAssemblyException()
+        {
+        }
+
+        public LoadAssemblyException(string message) : base(message)
+        {
+        }
+    }
+}
